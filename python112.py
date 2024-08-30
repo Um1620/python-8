ab50abf35c758a165c191392ea098b89e4c3f10e
usman <usmanmajeed8063@gmail.com>
2024-08-30 14:54:22 +0000

    python

---
+++ python112.py
@@ -0,0 +1,14 @@
+def age_calculate(num1:int ,num2:int ):
+    year_difference=current_year-birthday_year
+    print(year_difference)
+    return year_difference
+
+#input
+current_year=2024
+birthday_year=2004    
+
+output=age_calculate(2004,2024)
+print(output)
+    
+
+
